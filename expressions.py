8a39c67aa21e933683289dd02ef321cb0e53005c
mo-hajir <mohamedhajir36@gmail.com>
2024-04-13 19:45:24 +0200

    these are expressions

---
+++ expressions.py
@@ -0,0 +1,10 @@
+def add_numbers (num_a, num_b ):
+    return num_a + num_b 
+
+def add_one (num_a):
+    print(num_a + 1) 
+
+add_one(2) #Evaluates to 3
+add_one(3) #Evaluates to 4
+add_one(4) * 3 #Evaluates to 15
+(2 + add_one(4)) * 3 #Evaluates to 17
