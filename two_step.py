12420cd0dfe4dfb5b2e0a26788be97b14066e1c2
mo-hajir <mohamedhajir36@gmail.com>
2024-04-13 21:34:12 +0200

    these are my two step/one step

---
+++ two_step.py
@@ -0,0 +1,51 @@
+def add_one_and_divide_by_two_with_statements(num):
+  added = num + 1
+  halved = added / 2
+  return halved
+
+print("add_one_and_divide_by_two_with_statements(5) is:")
+
+print(
+  add_one_and_divide_by_two_with_statements(5)
+)
+def add_one_and_divide_by_two_with_an_expression(num):
+  return (num + 1) / 2
+
+print("add_one_and_divide_by_two_with_an_expression(5) is:")
+
+print(
+  add_one_and_divide_by_two_with_an_expression(5)
+)
+
+#Excersises
+
+print("")
+print("Function: divide_by_two_and_add_one")
+
+def divide_by_two_and_add_one(num):
+  divided = num / 2
+  added = divided + 1
+  return added
+
+
+#2
+
+print("")
+print("Function: multiply_by_forty_and_add_sixty")
+
+def multiply_by_forty_and_add_sixty(num):
+  multiplied = num * 40
+  added = multiplied + 60
+  return added
+  
+
+#3
+
+print("")
+print("Function: add_together_and_double")
+
+def add_together_and_double(num_a, num_b):
+  added = num_a + num_b
+  doubled = added * 2
+  return doubled
+
