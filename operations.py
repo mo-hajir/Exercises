5832387a14ca0292ffff65ccf009fba11cef1a08
mo-hajir <mohamedhajir36@gmail.com>
2024-04-13 19:13:20 +0200

    This is for operators and their symbols

---
+++ operations.py
@@ -0,0 +1,15 @@
+# Modulus
+modulus = 3 % 2
+print(f"3 % 2 = {modulus} (should be 1)")
+#Floor division
+floor_divided = 2 // 3
+print(f"2 // 3 = {floor_divided} (should be 0)")
+#Expotentiation
+expr = 2 ** 3
+print(f"2 ** 3 = {expr} (should be 8)")
+#Divisions
+divided = 2 / 3
+print(f"2 / 3 = {divided} (should be 0.6666666666666666)")
+#subtractions
+subtraction = 2 - 3
+print(f"2 - 3 = {subtraction} (should be -1)")
