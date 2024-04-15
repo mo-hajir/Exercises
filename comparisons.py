e0124ce14844bbeb52b98258ecc682952f5733eb
mo-hajir <mohamedhajir36@gmail.com>
2024-04-15 15:36:01 +0200

    comparison symbols

---
+++ comparisons.py
@@ -0,0 +1,29 @@
+#Exercise 1
+
+def a_is_less_than_b(a, b):
+  return a < b
+
+#Exercise 2
+
+def a_is_greater_than_b(a, b):
+   return a > b
+
+#Exercise 3
+
+def a_is_less_than_or_equal_to_b(a, b):
+ return a <= b
+
+#Exercise 4
+
+def a_is_greater_than_or_equal_to_b(a, b):
+   return a >= b
+
+#Exercise 5
+
+def a_is_not_equal_to_b(a, b):
+   return a != b
+
+#Exercise 6
+
+def a_is_within_b(a, b):
+   return a in b
