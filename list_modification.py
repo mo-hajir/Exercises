2afbaa510741ff5e04594112776bf7750c3f8388
mo-hajir <mohamedhajir36@gmail.com>
2024-04-15 21:56:03 +0200

    list modification

---
+++ list_modification.py
@@ -0,0 +1,28 @@
+#Exercise 1
+
+def remove_item_from_list(the_list, item):
+  the_list.remove(item)
+  return the_list
+
+#Exercise 2
+
+def get_index_of_item(the_list, item):
+  return the_list.index (item)
+
+#Exercise 3
+
+def count_items_in_list(the_list, item):
+  return the_list.count(item)
+
+#Exercise 4
+
+def reverse_list(the_list):
+  the_list.reverse()
+  return the_list
+
+#Exercise 5
+
+def list_length(the_list):
+  return len(the_list) 
+
+
