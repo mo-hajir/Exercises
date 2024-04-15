6e5f1f3e702d12a83ae3d15de6b03368a3e7661f
mo-hajir <mohamedhajir36@gmail.com>
2024-04-15 19:50:19 +0200

    this is my indexing for lists

---
+++ list_indexing.py
@@ -0,0 +1,19 @@
+#Exercise 1 
+
+def get_first_item(the_list):
+  return the_list[0]
+
+#Exercise 2
+
+def get_last_item(the_list):
+   return the_list[-1]
+
+#Exercise3
+
+def get_nth_item(the_list, n):
+   return the_list[n]
+
+#Exercise 4
+
+def get_items_between_one_and_three(the_list):
+   return the_list[1:3]
