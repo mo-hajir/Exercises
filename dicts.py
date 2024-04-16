328d862b773421cf35a37be0285d9bedd079c0e7
mo-hajir <mohamedhajir36@gmail.com>
2024-04-16 13:54:49 +0200

    dictionary

---
+++ dicts.py
@@ -0,0 +1,14 @@
+my_dictionary = {
+  "String": "A sequence of characters",
+  "List": "A sequence of any item",
+  "Dictionary": "A collection of keys mapped to values",
+}
+
+print("A String is:")
+print("  " + my_dictionary["String"])
+
+print("A List is:")
+print("  " + my_dictionary["List"])
+
+print("A Dictionary is:")
+print(" "+my_dictionary["Dictionary"])
